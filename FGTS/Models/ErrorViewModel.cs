478aab760f595491e9797e5f4199c0331051f743
offic <officialjla@gmail.com>
2022-12-22 13:44:03 +0800

    Add project files.

---
+++ FGTS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FGTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
