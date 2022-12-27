c146bcc36793631ea9e9ce42c6e7a3cf4b811d82
Carl0Bolante <98carlo.bolante@gmail.com>
2022-12-27 11:09:10 +0800

    Commit to pull

---
+++ FGTS/Controllers/ProvincesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FGTS.Controllers
+{
+    public class ProvincesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
